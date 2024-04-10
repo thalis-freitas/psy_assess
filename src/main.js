84977118d3a47332164f039ee3eecb38f0eb70d2
Thalis Freitas <ofthalis@gmail.com>
2024-04-10 19:58:57 -0300

    add material-design-icons

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 import { createVuestic } from 'vuestic-ui'
 import 'vuestic-ui/css'
+import 'material-design-icons-iconfont/dist/material-design-icons.min.css'
 
 const app = createApp(App)
 
