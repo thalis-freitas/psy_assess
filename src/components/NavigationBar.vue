1d9f17e205e5c79994272ef8018cb4c39a204e1c
Thalis Freitas <ofthalis@gmail.com>
2024-04-10 21:54:50 -0300

    create navigation bar

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,24 @@
+
+<script setup>
+  defineProps({
+    showSidebar: Boolean
+  })
+
+  defineEmits(['toggleSidebar'])
+</script>
+
+<template>
+  <va-navbar color="primary" class="py-2">
+    <template #left>
+      <va-button
+        :icon="$props.showSidebar ? 'menu_open' : 'menu'"
+        @click="$emit('toggleSidebar')"
+      />
+    </template>
+    <template #center>
+      <va-navbar-item class="font-bold text-lg">
+        PsyAssess
+      </va-navbar-item>
+    </template>
+  </va-navbar>
+</template>
