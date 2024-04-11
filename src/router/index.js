12f2e811cb099cda54d734046d9a0f8d81546832
Thalis Freitas <ofthalis@gmail.com>
2024-04-10 21:59:07 -0300

    add basic layout

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import AuthenticatedLayout from '@/components/AuthenticatedLayout.vue'
+import InstrumentsView from '@/views/InstrumentsView.vue'
+import EvaluetedView from '@/views/EvaluetedView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: AuthenticatedLayout,
+      children: [
+        {
+          path: '/',
+          name: 'instruments',
+          component: InstrumentsView
+        },
+        {
+          path: '/evalueted',
+          name: 'evalueted',
+          component: EvaluetedView
+        },
+      ]
     }
   ]
 })
