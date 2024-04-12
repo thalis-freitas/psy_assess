744a3fd97bc2d16226d46ea29aed641bdd814883
Thalis Freitas <ofthalis@gmail.com>
2024-04-11 22:00:57 -0300

    add tailwind.css

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import { createVuestic } from 'vuestic-ui'
 import 'vuestic-ui/css'
 import 'material-design-icons-iconfont/dist/material-design-icons.min.css'
+import './index.css'
 
 const app = createApp(App)
 
